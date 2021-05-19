034abe7121956c012d2d70540668ad6586579d45
thisisstreamliner <thisisstreamliner@gmail.com>
2021-05-19 12:49:32 +0300

    Getting Location Data From Across Platform

---
+++ lib/screens/city_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/utilities/constants.dart';
+
+class CityScreen extends StatefulWidget {
+  @override
+  _CityScreenState createState() => _CityScreenState();
+}
+
+class _CityScreenState extends State<CityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/city_background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: TextButton(
+                  onPressed: (){},
+                  child: Icon(Icons.arrow_back_ios, size: 50.0,),
+                ),
+              ),
+              const SizedBox(height: 20, width: 20,),
+              TextButton(
+                onPressed: (){}, 
+                child: Text(
+                  'Get Weather',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
